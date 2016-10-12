4b5747a8927209b631c06f6bd1b70fc94438c2d8
Andy Strominger <andrew.strominger@spidasoftware.com>
2016-10-12 08:01:39 -0400

    Change to not use iframes

--- jqueryupload.js
+++ jqueryupload.js
@@ -1,141 +1,125 @@
 /*
- * jQuery.upload v1.0.3
+ * jQuery.upload v1.0.2
  *
- * Copyright (c) 2013 Bass Jobsen
- * http://www.w3masters.nl/
+ * Copyright (c) 2010 lagos
  * Dual licensed under the MIT and GPL licenses.
  *
- * Original written by Lagos (http://lagoscript.org/)
- *
- * Contributors:
- * Mr Rogers http://rcode5.wordpress.com/
+ * http://lagoscript.org
  */
- 
+ // THIS HAS BEEN MODIFIED BY SPIDA, we set X-Frame-Options: DENY for every response so using iframes wasn't working.
+ // https://github.com/spidasoftware/jqueryupload
 (function($) {
 
-  var uuid = 0;
+        var uuid = 0;
 
-  $.fn.upload = function(url, data, callback, type) {
-    var self = this, inputs, checkbox, checked,
-    iframeName = 'jquery_upload' + ++uuid,
-    iframe = $('<iframe name="' + iframeName + '" style="position:absolute;top:-9999px" />').appendTo('body'),
-    form = '<form target="' + iframeName + '" method="post" enctype="multipart/form-data" />';
+        $.fn.upload = function(url, data, callback, type) {
+                if ($.isFunction(data)) {
+                        type = callback;
+                        callback = data;
+                        data = {};
+                }
 
-    if ($.isFunction(data)) {
-      type = callback;
-      callback = data;
-      data = {};
-    }
+                var formData = new FormData();
+                $.each($(this).find('input'), function(index, input) {
+                        var inputJq = $(input);
+                        var type = inputJq.attr('type');
+                        var name = inputJq.attr('name');
+                        if (name) {
+                                if(type === 'file' && inputJq[0].files.length > 0) {
+                                        formData.append(name,  inputJq[0].files[0]);
+                                } else if(type === 'checkbox' || type === 'radio') {
+                                        if(inputJq.is(':checked')) {
+                                                formData.append(name, 'on');
+                                        }
+                                } else {
+                                        formData.append(name, inputJq.val() ? inputJq.val().toString() : "");
+                                }
+                        }
+                });
 
-    checkbox = $('input:checkbox', this);
-    checked = $('input:checked', this);
-    form = self.wrapAll(form).parent('form').attr('action', url);
+                $.each(param(data), function(param) {
+                        formData.append(param.name, param.value);
+                });
 
-    // Make sure radios and checkboxes keep original values
-    // (IE resets checkd attributes when appending)
-    checkbox.removeAttr('checked');
-    checked.attr('checked', true);
+                $.ajax({
+                        url: url,
+                        data: formData,
+                        type: 'POST',
+                        contentType: false,
+                        processData: false,
+                        success: function(data) {
+                                var parsedData = handleData(data, type);
+                                callback.call(self, data);
+                                if (type === 'script') {
+                                        $.globalEval(parsedData);
+                                }
+                                if (callback) {
+                                        callback.call(self, parsedData);
+                                }
+                        }
+                });
+               
+                return this;
+        };
 
-    inputs = createInputs(data);
-    inputs = inputs ? $(inputs).appendTo(form) : null;
+        function param(data) {
+                if ($.isArray(data)) {
+                        return data;
+                }
+                var params = [];
 
-    form.submit(function() {
-      iframe.load(function() {
-	var data = handleData(this, type),
-	checked = $('input:checked', self);
+                function add(name, value) {
+                        params.push({name:name, value:value});
+                }
 
-	form.after(self).remove();
-	checkbox.removeAttr('checked');
-	checked.attr('checked', true);
-	if (inputs) {
-	  inputs.remove();
-	}
+                if (typeof data === 'object') {
+                        $.each(data, function(name) {
+                                if ($.isArray(this)) {
+                                        $.each(this, function() {
+                                                add(name, this);
+                                        });
+                                } else {
+                                        add(name, $.isFunction(this) ? this() : this);
+                                }
+                        });
+                } else if (typeof data === 'string') {
+                        $.each(data.split('&'), function() {
+                                var param = $.map(this.split('='), function(v) {
+                                        return decodeURIComponent(v.replace(/\+/g, ' '));
+                                });
 
-	setTimeout(function() {
-	  iframe.remove();
-	  if (type === 'script') {
-	    $.globalEval(data);
-	  }
-	  if (callback) {
-	    callback.call(self, data);
-	  }
-	}, 0);
-      });
-    }).submit();
+                                add(param[0], param[1]);
+                        });
+                }
 
-    return this;
-  };
+                return params;
+        }
 
-  function createInputs(data) {
-    return $.map(param(data), function(param) {
-    var e = $(document.createElement('input'));
-	e.attr('type', 'hidden');
-	e.attr('name', param.name);
-	e.attr('value', param.value);
-	return e;
-    });
-  }
+        function handleData(data, type) {
+                if ($.isXMLDoc(data) || data.XMLDocument) {
+                        return data.XMLDocument || data;
+                }
 
-  function param(data) {
-    if ($.isArray(data)) {
-      return data;
-    }
-    var params = [];
+                switch (type) {
+                        case 'xml':
+                                data = parseXml(data);
+                                break;
+                        case 'json':
+                                data = window.eval('(' + data + ')');
+                                break;
+                }
+                return data;
+        }
 
-    function add(name, value) {
-      params.push({name:name, value:value});
-    }
-
-    if (typeof data === 'object') {
-      $.each(data, function(name) {
-	if ($.isArray(this)) {
-	  $.each(this, function() {
-	    add(name, this);
-	  });
-	} else {
-	  add(name, $.isFunction(this) ? this() : this);
-	}
-      });
-    } else if (typeof data === 'string') {
-      $.each(data.split('&'), function() {
-	var param = $.map(this.split('='), function(v) {
-	  return decodeURIComponent(v.replace(/\+/g, ' '));
-	});
-
-	add(param[0], param[1]);
-      });
-    }
-
-    return params;
-  }
-
-  function handleData(iframe, type) {
-    var data, contents = $(iframe).contents().get(0);
-
-    if ($.isXMLDoc(contents) || contents.XMLDocument) {
-      return contents.XMLDocument || contents;
-    }
-    data = $(contents).find('body').text();
-    switch (type) {
-    case 'xml':
-      data = parseXml(data);
-      break;
-    case 'json':
-      data = $.parseJSON(data);
-      break;
-    }
-    return data;
-  }
-
-  function parseXml(text) {
-    if (window.DOMParser) {
-      return new DOMParser().parseFromString(text, 'application/xml');
-    } else {
-      var xml = new ActiveXObject('Microsoft.XMLDOM');
-      xml.async = false;
-      xml.loadXML(text);
-      return xml;
-    }
-  }
+        function parseXml(text) {
+                if (window.DOMParser) {
+                        return new DOMParser().parseFromString(text, 'application/xml');
+                } else {
+                        var xml = new ActiveXObject('Microsoft.XMLDOM');
+                        xml.async = false;
+                        xml.loadXML(text);
+                        return xml;
+                }
+        }
 
 })(jQuery);
